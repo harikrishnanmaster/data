87eac1bbc8ff8a87261d81112ac9268eb775a897
Harikrishnan K <harikrishnanmaster@gmail.com>
2018-01-23 15:36:23 +0530

    Update test.py

--- spiders/test.py
+++ spiders/test.py
@@ -7,131 +7,145 @@ from bs4 import BeautifulSoup
 from project.items import ProjectItem
 import re
 from pattern.web import plaintext
+from scrapy.selector import HtmlXPathSelector
 import requests
+from scrapy.selector import Selector
 class PagesSpider(CrawlSpider):
- """
+    """
     the Page Spider for wikipedia
     """
 
- name = "project"
+    name = "pro"
     #allowed_domains = ["wikipedia.org"]
 
- start_urls = ['https://www.blablacar.in/ride-sharing/new-delhi/chandigarh/?fn=new+delhi&fcc=IN&tn=chandigarh&tcc=IN&sort=trip_date&order=asc&limit=10&page=%s' % page for page in xrange(1,15)]
+    start_urls = ['https://www.blablacar.in/ride-sharing/new-delhi/chandigarh/?fn=new+delhi&fcc=IN&tn=chandigarh&tcc=IN&sort=trip_date&order=asc&limit=10&page=%s' % page for page in xrange(1,15)]
     
 
- rules = (
+    rules = (
         Rule(LinkExtractor(),
              callback='parse_page',
              # hook to be called when this Rule generates a Request
              process_request='parse'),
     )
 
-    
-    
+   
+    def parse(self, response):
+     sel = Selector(response)
+     #hxs = HtmlXPathSelector(response)
+     #li = hxs.xpath("//a[@class='trip-search-oneresult js-tracktor-search-result']")
+     f = open('csvfile.csv','a')
+     for href in sel.xpath("//a[@class='trip-search-oneresult js-tracktor-search-result']/@href").extract():
+      a = "https://www.blablacar.in%s"%href
+      
+      
+      #print a 
+      #for l in a:
+      r = requests.get(a)
+      hxs = HtmlXPathSelector(r)
+      hxs2 = Selector(r)
+      #for source in hxs2.xpath("//span[@class='RideName-location RideName-location--arrowAfter']/text()").extract():
+      source = hxs.xpath("//span[@class='RideName-location RideName-location--arrowAfter']/text()").extract()[0]
+      if source:
 
- def parse(self, response):
-  item = ProjectItem()
-  soup = BeautifulSoup(response.body,"html.parser")
-  li = soup.find_all("a", {"class": "trip-search-oneresult js-tracktor-search-result"})
+       print '\nsource : %s'%source.strip()
+       f.write('\nsource : %s'%source.strip())
+      else:
+       pass
+      destination =  hxs.xpath("//span[@class='RideName-location RideName-location--arrowAfter']/following-sibling::node()/text()").extract()[0]
+      if destination:
+       print destination.strip()
+      
+       f.write('\ndestination : %s'%destination.strip())
+      else:
+       pass
+      for prices in hxs.xpath("//span[@class='Booking-price u-block'][1]/text()").extract():
+        price =  prices[1:-1]
+        print '\nprice : %s'%price.strip()
+        f.write('\nprice : %s'%price.strip())
+      else:
+        pass
+      departuredate = hxs.xpath("//strong[@class='RideDetails-infoValue']/span/text()").extract()[0]
+      if departuredate:
+       print '\ndeparturedate : %s' %departuredate.strip()
+       f.write('\ndeparturedate : %s'%departuredate.strip())
+      else:
+       pass
+      departurepoint = hxs.xpath("//span[@class='RideDetails-infoValue RideDetails-infoValue--clickable tip js-display-map']/span/text()").extract()
+      if departurepoint:
+       d = departurepoint[0]
+       print '\ndeparturepoint : %s'%d.strip()
+       f.write('\ndeparturepoint : %s'%d.strip())
+      else:
+       pass
+      departure = hxs.xpath("//span[@class='RideDetails-infoValue RideDetails-infoValue--clickable tip js-display-map']/span/text()").extract()
+      if departure:
+       d2 = departure[1]
+       print '\ndropoffpoint : %s'%d2.strip()
+       f.write('\ndropoffpoint : %s'%d2.strip())
+      else:
+       pass
+      avail = hxs.xpath("//span[@class='Booking-seats u-block']//text()").extract()[0]
+      if avail:
+       print '\nseats_left : %s'%avail.strip()
+       f.write('\nseats_left : %s'%avail.strip())
+      else:
+       pass
+      photo = hxs.xpath("//a[@class='PhotoWrapper PhotoWrapper--medium']/@href").extract()[0]
+      if photo:
+       print '\ncar_owner_image  : https://d2kwny77wxvuie.cloudfront.net%s'%photo.strip()
+       f.write('\ncar_owner_image  :  https://d2kwny77wxvuie.cloudfront.net%s'%photo.strip())
+      else:
+       pass
+      name = hxs2.xpath("//h4[@class='ProfileCard-info ProfileCard-info--name u-truncate']/a/text()").extract()[0]
+      if name:
+       print '\ncar_owner_name  : %s'%name.strip()
+       f.write('\ncar_owner_name  : %s'%name.strip())
+      else:
+       pass
+      age = hxs2.xpath("//div[@class='ProfileCard-info']//text()").extract()[0]
+      if age:
+       print '\ncar_owner_age  : %s'%age.strip()
+       f.write('\ncar_owner_age  : %s'%age.strip())
+      else:
+       pass
+      car = hxs.xpath("//p[@class='Profile-carDetails u-cell']/text()").extract()
+      if car:
+       carz = car[0]
+       care = car[1]
+       print '\ncar_model : %s'%carz.strip()
+       print '\n%s'%care.strip()
+       f.write('\ncar_model :%s'%carz.strip())
+       f.write('\n%s'%care.strip())
+      #for c in care:
+      else:
+        pass
+       #print c.strip()
+       #print carz.strip()
+       #f.write('\ncardetails : %s'%c.strip())
+      #else:
+       #pass
+      stars = hxs.xpath("//span[@class='u-textBold u-darkGray']/text()").extract()
+      for s in stars:
+       
+       print '\ncar_owner_rating : %s'%s
+       f.write('\ncar_owner_rating : %s'%s.strip())
+      else:
+       pass
+      options = hxs.xpath("//div[@class='RideDetails-infoValue']/span/text()").extract_first()
+      
+      op = options
+      if op:
+        print '\noptions : %s'%op.strip()
+        f.write('\noptions : %s'%op.strip())
+
+      else:
+        pass
+      
+      #for o in options:
  
- #links = li.find("a", {"class": "trip-search-oneresult js-tracktor-search-result"})
-  f = open('csvfile.csv','a')
-  for l in li:
-   #print "--------------------------------------------------------------------------------------------------------------"
-   a = 'https://www.blablacar.in'+l.get('href')
-   r2 = requests.get(a)
-   soup2 = BeautifulSoup(r2.content,"html.parser")
-   
-   #print "'%s'"%a
-   #for b in a:
-    #r2 = requests.get(b)
-    #soup2 = BeautifulSoup(r2.content,"html.parser")
-    
-   #links = soup2.find_all("div", {"class": "RideDetails trip-container"})
-   
-   #for link in links:
-    
-   
-   try:
-     source = soup2.find("span",{"class":"RideName-location RideName-location--arrowAfter"}).get_text()
-     print ('\nsource : %s'%source.strip())
-     f.write('\nsource : %s'%source.strip())
-   except:
-     pass
-   try:
-     destination = soup2.find("span",{"class":"RideName-location RideName-location--arrowAfter"}).next_sibling.get_text()
-     print ('\ndestination : %s'%destination.strip())
-     f.write('\ndestination : %s'%destination.strip())
-   except:
-     pass
-   try:
-     price = soup2.find("span",{"class":"Booking-price u-block"}).text[1:-1]
-     print ('\nprice : %s'%price.strip())
-     f.write('\nprice : %s'%price.strip())
-     #print "departurepoint: %s"%(l.find("dd",{"class":"js-tip-custom"}).get_text())
-     #print "departurepoint: %s"%(l.find("h3",{"class":"time u-darkGray"}).get_text())
-     #print i
-   except:
-     pass
-   try:
-     departuredate = soup2.find("strong",{"class":"RideDetails-infoValue"}).get_text()
-     print ('\ndeparturedate : %s'%departuredate.strip())
-     f.write('\ndeparturedate : %s'%departuredate.strip())
-   except:
-     pass
-   try:
-     departurepoint = soup2.find("span",{"class":"RideDetails-infoValue RideDetails-infoValue--clickable tip js-display-map"}).get_text()
-     print ('\ndeparturepoint : %s'%departurepoint.strip())
-     f.write('\ndeparturepoint : %s'%departurepoint.strip())
-   except:
-     pass
-   try:
-     departure = soup2.find_all("span",{"class":"RideDetails-infoValue RideDetails-infoValue--clickable tip js-display-map"})[1].get_text()
-     print ('\ndropoffpoint : %s'%departure.strip())
-     f.write('\ndropoffpoint : %s'%departure.strip())
-   except:
-     pass
-   try:
-     avail = soup2.find("span",{"class":"Booking-seats u-block"}).get_text()
-     print ('\navailableseats : %s'%avail.strip())
-     f.write('\navailableseats : %s'%avail.strip())
-   except:
-     pass
-   try:
-     photo = soup2.find("a",{"class":"PhotoWrapper PhotoWrapper--medium"}).get('href')
-     print ('\nphoto : %s'%photo.strip())
-     f.write('\nphoto : %s'%photo.strip())
-   except:
-     pass
-   try:
-     name = soup2.find("h4",{"class":"ProfileCard-info ProfileCard-info--name u-truncate"}).get_text()
-     print ('\nname : %s'%name.strip())
-     f.write('\nname : %s'%name.strip())
-   except:
-     pass
-   try:
-     age = soup2.find("div",{"class":"ProfileCard-info"}).get_text()
-     print ('\nage : %s'%age.strip())
-     f.write('\nage : %s'%age.strip())
-   except:
-     pass
-   try:
-     stars = soup2.find("p",{"class":"ratings-container"}).get_text()
-     print ('\nstars : %s'%stars.strip())
-     f.write('\nstars : %s'%stars.strip())
-   except:
-     pass
-   try:
-     car = soup2.find("p",{"class":"Profile-carDetails u-cell"}).get_text()
-     print ('\ncardetails : %s\n'%car.strip())
-     f.write('\ncardetails : %s'%car.strip())
-   except:
-     pass
-   try:
-     options = soup2.find("div",{"class":"RideDetails-infoValue"}).get_text()
-     print ('\noptions : %s'%options.strip())
-     f.write('\noptions : %s'%options.strip())
-   except:
-     pass
-  f.close()   
-  
+       #print o[0]
+       #f.write('\noptions : %s'%o.strip())
+      #else:
+       #pass
+     f.close()
+     
